62f40a16faaa3cc368bae3f831300c491066eb3c
DancingLeaf <851280706@qq.com>
2024-02-26 01:19:57 +0800

    二级分类-定制路由滚动行为

--- vue-rabbit/src/router/index.js
+++ vue-rabbit/src/router/index.js
@@ -1,39 +1,45 @@
 // createRouter：创建router实例对象
 // createWebHistory：创建history模式的路由
-import { createRouter, createWebHistory } from 'vue-router'
-import Login from '@/views/Login/index.vue'
-import Layout from '@/views/Layout/index.vue'
-import Category from '@/views/Category/index.vue'
-import Home from '@/views/Home/index.vue'
-import SubCategory from '@/views/SubCategory/index.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Login from "@/views/Login/index.vue";
+import Layout from "@/views/Layout/index.vue";
+import Category from "@/views/Category/index.vue";
+import Home from "@/views/Home/index.vue";
+import SubCategory from "@/views/SubCategory/index.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   // 配置path和component的映射关系
   routes: [
     {
-      path: '/',
+      path: "/",
       component: Layout,
       children: [
         {
-          path: '',
-          component: Home
+          path: "",
+          component: Home,
         },
         {
-          path: 'category/:id',
-          component: Category
+          path: "category/:id",
+          component: Category,
         },
         {
-          path: 'category/sub/:id',
-          component: SubCategory
-        }
-      ]
+          path: "category/sub/:id",
+          component: SubCategory,
+        },
+      ],
     },
     {
-      path: '/login',
-      component: Login
-    }
-  ]
-})
+      path: "/login",
+      component: Login,
+    },
+  ],
+  // 路由滚动行为定制
+  scrollBehavior() {
+    return {
+      top: 0,
+    };
+  },
+});
 
-export default router
+export default router;
